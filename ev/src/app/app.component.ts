6aa086117c930e1eae77e5d156c0c56e38777362
Luis Miguel Fernández Costa <luismiguel.fernandez@murciaeduca.es>
2021-02-15 15:12:48 +0100

    routing añadido

--- ev/src/app/app.component.ts
+++ ev/src/app/app.component.ts
@@ -8,31 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
-  coches;
-  title = 'ev';
-  vistaBigCard = true;
-  loading = false;
-  patron = "";
->>>>>>> 6612e3c9433d5b36ca14c5a48290562d6c89f9be
 
   constructor() {
   }
   
   ngOnInit() {
 
-<<<<<<< HEAD
   }
 
 
-=======
-  cambiarVista() {
-    this.vistaBigCard = !this.vistaBigCard
-  }
-
-  filtrarCoches($event) {
-    this.patron = $event
-  }
->>>>>>> 6612e3c9433d5b36ca14c5a48290562d6c89f9be
 }
